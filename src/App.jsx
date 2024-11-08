32a0860bd69fdc5777da1533a3662d523bab542f
coderseyfi <seyfaddinmn@code.edu.az>
2024-11-08 16:47:54 +0400

    first

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Lo gin />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>

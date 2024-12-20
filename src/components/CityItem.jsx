e3605e300c8da9fe92cac658611ec6e65befaab8
coderseyfi <seyfaddinmn@code.edu.az>
2024-12-20 11:15:47 +0400

    Finishing the city view

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -10,16 +11,15 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  console.log(city);
-
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
-
-  console.log(position);
 
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
